93603b32f473dee811bd1256931d6fc475a0af1b
OldTyT <nezlobin118@gmail.com>
2023-06-12 11:23:45 +0000

    feat: added proxy

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ from loguru import logger as my_logger
 
 from models.runtime import RuntimeSettings
 from jobs import GenerateTextWithGPTModel
+from proxy import ProxyMessage
 
 
 bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
 dp = Dispatcher(bot)
+pm = ProxyMessage()
+
 
 redis_conn = Redis(
     host = str(os.getenv("REDIS_HOST", "localhost")),
@@ -34,44 +37,50 @@ state_cfg = RuntimeSettings(
 
 
 def prompt_gpt4_start(prompt, chat_id):
-    my_logger.debug(f"Setup gpt4_prompt_job. Chat id: {chat_id}. Prompt: {prompt}")
-    gpt4_prompt_job = state_cfg.rq_queue.enqueue(
-        GenerateTextWithGPTModel,
-        chat_id=chat_id,
-        prompt=prompt,
-        tg_bot_token=SecretStr(os.getenv("TELEGRAM_BOT_TOKEN")),
-        job_timeout=120,
-        retry=Retry(max=3)
-    )
+    if pm.check(message):
+        my_logger.debug(f"Setup gpt4_prompt_job. Chat id: {chat_id}. Prompt: {prompt}")
+        gpt4_prompt_job = state_cfg.rq_queue.enqueue(
+            GenerateTextWithGPTModel,
+            chat_id=chat_id,
+            prompt=prompt,
+            tg_bot_token=SecretStr(os.getenv("TELEGRAM_BOT_TOKEN")),
+            job_timeout=120,
+            retry=Retry(max=3)
+        )
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    msg_start = "GPT4 FastBot\n" \
-                "\n" \
-                "Ask me something. Or use cmd `/gen <something>`."
-    await message.answer(msg_start, parse_mode="Markdown")
+    if pm.check(message):
+        msg_start = "GPT4 FastBot\n" \
+                    "\n" \
+                    "Ask me something. Or use cmd `/gen <something>`."
+        my_logger.debug(f"Starting for chat id: {message.chat.id}")
+        await message.answer(msg_start, parse_mode="Markdown")
 
 
 @dp.message_handler(commands=['ping'])
 async def message_ping_cmd(message: types.Message):
-    await message.answer("pong")
+    if pm.check(message):
+        await message.answer("pong")
 
 
 @dp.message_handler(commands=['gen'])
 async def message_gen_cmd(message: types.Message):
-    if len(message.text) < 5:
-        await message.answer(f"The text is very short.")
-        return
-    prompt_gpt4_start(
-        prompt=message.text[5:],  # remove '/gen ' from msg
-        chat_id=message.chat.id
-    )
+    if pm.check(message):
+        if len(message.text) < 5:
+            await message.answer(f"The text is very short.")
+            return
+        prompt_gpt4_start(
+            prompt=message.text[5:],  # remove '/gen ' from msg
+            chat_id=message.chat.id
+        )
 
 
 @dp.message_handler()
 async def all_message_handler(message: types.Message):
-    prompt_gpt4_start(prompt=message.text, chat_id=message.from_user.id)
+    if pm.check(message):
+        prompt_gpt4_start(prompt=message.text, chat_id=message.from_user.id)
 
 
 if __name__ == '__main__':
---
+++ proxy.py
@@ -0,0 +1,6 @@
+from loguru import logger
+
+class ProxyMessage:
+    def check(self, message):
+        logger.debug(message)
+        return True
