b7286c7cd98849b261bdf1e676e7b0b9ea7f4a27
OldTyT <nezlobin118@gmail.com>
2023-06-27 05:17:51 +0000

    fix: exntypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ "$PROGRAM_TYPE" = "worker" ]; then
-    python3 worker.py
+    python3 worker.py && exit
 fi
 if [ "$PROGRAM_TYPE" = "notify" ]; then
-    python3 notify.py
+    python3 notify.py && exit
 fi
 if [ "$PROGRAM_TYPE" = "bot" ]; then
     python3 db_init.py
-    python3 main.py
+    python3 main.py && exit
 else
     echo "Invalid env value - $PROGRAM_TYPE"
     exit 1
