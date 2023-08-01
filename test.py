7bbea0474e189af994714e0480d7a32073d40067
OldTyT <nezlobin118@gmail.com>
2023-08-01 13:20:45 +0000

    chore: fix lint

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import g4f
 
 response = g4f.ChatCompletion.create(model='gpt-4', provider=g4f.Provider.Bing, messages=[
-                                     {"role": "user", "content": "Hello world"}],stream=True)
+                                     {"role": "user", "content": "Hello world"}], stream=True)
 
 for message in response:
     print(message)
